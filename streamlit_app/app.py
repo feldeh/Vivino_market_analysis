7620cba7e46324ea972e193f55667638a31926b3
andreia-negreira <andreia_heringer@hotmail.com>
2023-08-31 09:33:44 +0200

    app.py modified

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -26,13 +26,3 @@ for i, row in df.iterrows():
 
 st_data = st_folium(m, width=1250)
 
-# Close the database connection
-conn.close()
-
-
-options = st.multiselect(
-    'What are your favorite colors',
-    ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red'])
-
-# st.write('You selected:', options)
