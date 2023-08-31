dc16a2f5f6f8ca0eb930edafd104dc5daca491a2
andreia-negreira <andreia_heringer@hotmail.com>
2023-08-31 15:44:02 +0200

    csv_grape_wine app_q.5

---
+++ streamlit_app/app_q_5.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from streamlit_agraph import agraph, Node, Edge, Config
+import json
+
+st.title("Exploring the Finest: 3 Top Grape Varieties and Their 5 Best Wines")
+
+
+df = pd.read_csv("/Users/andre/Documents/GitHub/Vivino_market_analysis/data/csv_streamlit/grape_wine.csv")
+
+
+unique_grapes = df.grape.unique()
+unique_wines = df.wines_names.unique()
+
+json_df = df.to_json(orient="values")
+dict_converted = json.loads(json_df)
+
+nodes = []
+edges = []
+grapes = []
+wines = []
+
+for data in dict_converted:
+    type_grape = data[4]
+    wines_names = data[1]
+    rank = data[2]
+    
+    if wines_names not in wines:
+        nodes.append(Node(id=wines_names, label=str(rank), shape='CURVE_SMOOTH', color='#FDD00F'))
+        wines.append(wines_names)
+    
+    if type_grape not in grapes:
+        nodes.append(Node(id=type_grape, label=type_grape, shape='CURVE_SMOOTH', color='#07A7A6'))
+        grapes.append(type_grape)
+    
+    edges.append(Edge(source=wines_names, target=type_grape))
+
+
+config = Config(width=750,
+                height=950,
+                directed=True, 
+                physics=True, 
+                hierarchical=False,
+                nodeHighlightBehavior=True, 
+                highlightColor="#F7A7A6",
+                collapsible=True,
+                node={'labelProperty':'label'}
+                )
+
+return_value = agraph(nodes=nodes, 
+                      edges=edges, 
+                      config=config)
+
+
+df_cabernet = pd.read_csv("/Users/andre/Documents/GitHub/Vivino_market_analysis/data/csv_streamlit/cabernet_result.csv")
+st.bar_chart(df_cabernet)
